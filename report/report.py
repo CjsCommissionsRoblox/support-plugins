2a9ec5d6231a7a7ef6f62c48fbcf3dd6a88e6347
CjsCommissionsRoblox <cjscommisions@outlook.com>
2025-03-18 21:50:54 +0000

    Update report.py

--- report/report.py
+++ report/report.py
@@ -41,13 +41,13 @@ class Report(commands.Cog):
                 return
 
             ellipsis = '...' if len(message.content) > 200 else ''
-            description = f'{message.content[:200]}{ellipsis}\n[Read More]({message.jump_url})'
+            description = f'{message.content[:200]}{ellipsis}\n[Jump to message]({message.jump_url})'
 
             em = discord.Embed(title='A new report has been sent in.', description=description, color=int("7FD957", 16), timestamp=discord.utils.utcnow())
             em.set_author(name=str(message.author), icon_url=message.author.display_avatar)
             em.set_footer(text=f'Reported by {author}', icon_url=author.display_avatar)
             em.add_field(name="User Info", value=f"User Id ``{message.author.id}`` Username ``{message.author}``")
-            us = discord.Embed(title='Thank you for your report!', description=f"You reported {description}", color=int("7FD957", 16), timestamp=discord.utils.utcnow())
+            us = discord.Embed(title='Thank you for your report!', description=f"You reported `{description}`", color=int("7FD957", 16), timestamp=discord.utils.utcnow())
             em.set_author(name=str(message.author), icon_url=message.author.display_avatar)
             await config.channel.send("<@&1350802603289874497>", embed=em)
             await author.send(embed=us)
