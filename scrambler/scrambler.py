afb2b14ff5f6dd1e071128fb6fb090a31e5d1f46
CjsCommissionsRoblox <cjscommisions@outlook.com>
2025-03-26 22:33:37 +0000

    Update scrambler.py

--- scrambler/scrambler.py
+++ scrambler/scrambler.py
@@ -4,22 +4,27 @@ import string
 from discord.ext import commands
 
 class ScrambleCog(commands.Cog):
+    """Scrambles a given word and returns a randomized version."""
+    
     def __init__(self, bot):
         self.bot = bot
 
-    # Scramble function with salt and randomization
     def scramble_word(self, word):
+        """Scrambles the given word with a salt for added complexity."""
         salt = ''.join(random.choices(string.ascii_letters + string.digits, k=5))  # Salt added
         word_with_salt = word + salt
         scrambled = ''.join(random.sample(word_with_salt, len(word_with_salt)))  # Shuffle letters
         return scrambled
 
-    # Scramble command
     @commands.command(name='scramble')
     async def scramble(self, ctx, *, word: str):
-        scrambled_word = self.scramble_word(word)
-        await ctx.send(f"Original word: {word}\nScrambled word: {scrambled_word}")
+        """Scrambles the word provided by the user."""
+        if word:
+            scrambled_word = self.scramble_word(word)
+            await ctx.send(f"Original word: {word}\nScrambled word: {scrambled_word}")
+        else:
+            await ctx.send("Please provide a word to scramble!")
 
-# Setup function to add the cog
-def setup(bot):
-    bot.add_cog(ScrambleCog(bot))
+async def setup(bot):
+    """Sets up the ScrambleCog in the bot."""
+    await bot.add_cog(ScrambleCog(bot))
